d39cffe35ecea5db21813616932e2a125e5cd53a
Jung Hangyeol <66053034+kmuhan@users.noreply.github.com>
2023-02-11 14:47:58 +0000

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Router from './Router';
 
 function App() {
   return (
-    <Router />
+    <GlobalStyle>
+      <Router />
+    </GlobalStyle>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: "Helvetica", "Arial", sans-serif;
+  line-height: 1.5;
+  background-color: #333333;
+}
+`
 
 export default App;
